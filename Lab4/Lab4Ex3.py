73e54d45e83e5b17fa0433136e69371010f0024e
Sefki Kolozali <skoloz@essex.ac.uk>
2018-11-21 10:33:47 +0000

    Revert "Lab4 and Lab5"
    This reverts commit 0d5b186a46e84d6222b7f6c023d1dd7528d4873b.

--- Lab4/Lab4Ex3.py
+++
@@ -1,8 +0,0 @@
-
-gotOne = False
-while not gotOne :
-  try :
-    age = int(input("Enter your age:"))
-    gotOne = True
-  except ValueError as e :
-    print("Invalid input – try again")
--- Lab4/Lab4Ex6.py
+++
@@ -1,6 +0,0 @@
-myString= "Test"
-f2 = open("myfile2.txt", 'w')
-f3 = open("file2.txt", 'a')
-f2.write("Hello")
-f3.write(myString)
-
